7dcd75f97b702dcc4e0eb8b74f7242bc0622ec5b
Rohan2035 <rohan12gupta9@gmail.com>
2022-03-11 22:19:05 +0530

    Tiny Fixes 🔧🔧

--- static/JavaScript/Script2.js
+++ static/JavaScript/Script2.js
@@ -6,5 +6,5 @@ x = document.querySelectorAll(".box");
 
 for (let i = 0; i < x.length; i ++) {
   s = `${x[i].textContent}`;
-  x[i].innerHTML = s.slice(0, 150) + '...';
+  x[i].innerHTML = s.slice(0, 97) + '...';
 }
